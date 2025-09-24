20c477d268d1e0b8932b90b7e43aebcf4d7c813d
Joel Bernardes <119889298+JoelBernardes@users.noreply.github.com>
2025-09-24 14:05:28 -0400

    Updated furniture code
    Belongs with last commit as well

--- Objects/Furniture.gd
+++ Objects/Furniture.gd
@@ -40,6 +40,6 @@ func is_idle(delta:float) -> void:
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Immovable Object"):
-		body.collision_layer = 2
-		body.collision_mask = 2
+		##add code so player is not able to move and object cannot leave
+		pass
 	pass # Replace with function body.
