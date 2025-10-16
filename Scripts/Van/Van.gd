f69aafa263863c18ec9d3f3c65f8a7cd2973d3a3
ashtriffitt <ashtontriffitt813@gmail.com>
2025-10-16 01:10:20 -0400

    improved van in testlevel

--- Scripts/Van/Van.gd
+++ Scripts/Van/Van.gd
@@ -1,19 +1,24 @@
 extends Area2D
 
 var playerInArea:bool
-
+@export var vanDoor:Area2D
 
 
 func _process(delta:float)  -> void:
 	if (Input.is_action_just_pressed("Lift") && playerInArea):
 		print("pressed")
-		var bodies:Array[Node2D] = self.get_overlapping_bodies()
-		print(bodies.size())
-		
+		clearVan()
+
+func clearVan() -> void:
+	# gets all furniture in van
+	var bodies:Array[Node2D] = self.get_overlapping_bodies()
+	#print(bodies.size())
+	
+	if (!vanDoor.has_overlapping_bodies()):
+		print("door good")
 		for i in range(bodies.size()):
 			if bodies[i].is_in_group("Furniture"):
 				bodies[i].queue_free()
-
 
 func _on_button_area_area_entered(area: Area2D) -> void:
 	print("in button")
