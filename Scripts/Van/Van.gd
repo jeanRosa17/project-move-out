576e4bae5d0c4f2b7a740a86c9a3937fb459cdb6
ashtriffitt <ashtontriffitt813@gmail.com>
2025-10-16 01:19:38 -0400

    van 2.0

--- Scripts/Van/Van.gd
+++ Scripts/Van/Van.gd
@@ -2,8 +2,13 @@ extends Area2D
 
 var playerInArea:bool
 @export var vanDoor:Area2D
+@export var furniture:Array[Node]
 
+@export var score:float
 
+func _ready() -> void:
+	furniture = get_tree().get_nodes_in_group("Furniture")
+	
 func _process(delta:float)  -> void:
 	if (Input.is_action_just_pressed("Lift") && playerInArea):
 		print("pressed")
@@ -14,11 +19,15 @@ func clearVan() -> void:
 	var bodies:Array[Node2D] = self.get_overlapping_bodies()
 	#print(bodies.size())
 	
+	var totalFurniture:int = furniture.size()
+	
 	if (!vanDoor.has_overlapping_bodies()):
 		print("door good")
 		for i in range(bodies.size()):
 			if bodies[i].is_in_group("Furniture"):
 				bodies[i].queue_free()
+				score = totalFurniture / furniture.size()
+				print(score)
 
 func _on_button_area_area_entered(area: Area2D) -> void:
 	print("in button")
