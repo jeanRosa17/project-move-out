7bd29c70a1c71b063f00696245a40778ba953a63
Luna Rosario <jeanrosario1703@gmail.com>
2025-10-23 11:11:20 -0400

    Added in a Fade out for the title screen + made it the main scene

--- UI/TitleScreen.gd
+++ UI/TitleScreen.gd
@@ -1,54 +1,23 @@
-extends State
+extends Node2D
 
-@export var controller = null
+@onready var play_tutorial_a: Button = $"Container/BoxContainer/Panel/Play Tutorial A"
+@onready var play_tutorial_b: Button = $"Container/BoxContainer/Panel/Play Tutorial B"
+@onready var settings: Button = $Container/BoxContainer/Panel/Settings
+@onready var fader: ColorRect = $Fader
 
-## Determines if the state can be entered. By default returns true.
-func canEnter() -> bool:
-	return true
 
-## The first method called when the state is transitioned into
-func enter() -> void:
-	pass
+var tween:Tween = null
 
-## Determines if the state can be exited. By default returns true.
-func canExit() -> bool:
-	return true
 
-## The last method called when the state is transitioned out of
-func exit() -> void:
-	pass
-	
-## Constantly checks for input from the user and changes state.
-func update(_delta:float) -> void:
-	pass
+func _on_play_tutorial_a_pressed() -> void:
+	self.changeSceneTo("res://Levels/Scenes/Tutorial/Tutorial_Lobby.tscn")
 
-## This method runs every _physics_process() frame of the StateManager.
-func physicsUpdate(delta:float) -> void:
-	pass
 
-## Disables this State. Unlike canEnter, this function's job is to be a sure fire way to prevent a
-## State from being accessed at all.
-func disable() -> void:
-	self._isEnabled = false
-	self.onDisabled.emit()
+func _on_play_tutorial_b_pressed() -> void:
+	self.changeSceneTo("res://Levels/Tests/TestLevel.tscn")
 
-## Reenables this State
-func enable() -> void:
-	self._isEnabled = true
-	self.onEnabled.emit()
-
-## Returns whether this State is enabled
-func isEnabled() -> bool:
-	return self._isEnabled
-
-## Returns the State's name in addition to the time that the function was called.
-func logStatus() -> String:
-	return str(self.name + " @" + Time.get_time_string_from_system() + "|")
-	
-## Returns the StateManager that this State should belong to.
-func getManager() -> StateManager:
-	if (not self.get_parent() is StateManager):
-		Exception.new("The parent of a State should be a StateManager.")
-	
-	return self.get_parent()
-
+func changeSceneTo(filePath:StringName) -> void:
+	tween = null
+	tween = get_tree().create_tween()
+	tween.tween_property(fader, "modulate:a", Color.BLACK.a, 1.0).from(0.0)
+	tween.tween_callback(get_tree().change_scene_to_file.bind(filePath))
