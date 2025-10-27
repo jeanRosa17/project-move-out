f85591d80cdcad2cebaf005064864e53789c4caf
ashtriffitt <ashtontriffitt813@gmail.com>
2025-10-27 12:12:25 -0400

    few level 1 fixes

--- Levels/Tests/FurnitureAudio.gd
+++ Levels/Tests/FurnitureAudio.gd
@@ -13,7 +13,7 @@ func push_sound(furniture:Furniture) -> void:
 		play(0.3)
 		await get_tree().create_timer(.05).timeout
 		stop()
-		if (furniture.isPushing):
+		if (furniture.isPushing && furniture.linear_velocity.length() > 0):
 			push_sound(furniture)
 		## recursively play different pieces of audio at different pitches
 
