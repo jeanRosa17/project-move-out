5b747e8bb732ec92fab8300bfd138b6e85a36ac9
ashtriffitt <ashtontriffitt813@gmail.com>
2025-11-19 23:02:40 -0500

    ice system 1.3

--- Levels/Scenes/ice_areas.gd
+++ Levels/Scenes/ice_areas.gd
@@ -2,21 +2,37 @@ extends Area2D
 
 var player:Player
 
+var velocityCheck:bool = false
+
 @export var soundPlayer:AudioStreamPlayer2D
+
+func _process(float) -> void:
+	if (velocityCheck):
+		if (player.velocity.length() < .2 && !player.canControl):
+			player.onControls()
+		elif (player.canControl):
+			player.offControls()
+
 
 func _on_body_entered(body: Node2D) -> void:
 	# Make it so player always has the same velocity when on ice
 	if (body.name == "Player"):
 		player = body
-		player.offControls()
-		print(player.manager.direction)
-		player.manager.changeState("Move")
-		player.velocity = player.manager.direction
+		if (player.canControl):
+			player.offControls()
+			player.manager.changeState("Move")
+			player.velocity = player.manager.direction
 
-		soundPlayer.play()
+			soundPlayer.play()
+		
+			print("PLAYER ENTERED")
+		
+			velocityCheck = true
 
 
 func _on_body_exited(body: Node2D) -> void:
 		if (body.name == "Player"):
 			player.onControls()
 			soundPlayer.stop()
+			
+			velocityCheck = false
