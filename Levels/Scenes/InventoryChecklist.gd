d13b349e857c385f2a355546aa24c92bb60f97d8
ashtriffitt <ashtontriffitt813@gmail.com>
2025-12-09 12:45:05 -0500

    joel inventory system

---
+++ Levels/Scenes/InventoryChecklist.gd
@@ -0,0 +1,19 @@
+extends ItemList
+
+@export var needed:Array[Furniture]
+
+var isPressed:bool = false
+
+func _ready() -> void:
+	for i in range(needed.size()):
+		self.add_item(needed[i].name, needed[i].sprite_2d.texture)
+		
+
+func _process(float) -> void:
+	if (Input.is_action_pressed("Inventory")):
+		var layer:CanvasLayer = self.get_parent()
+		if (layer.visible):
+			layer.visible = false
+		else:
+			layer.visible = true
+	
