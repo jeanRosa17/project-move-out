a253d73baf5318465a107067db2c80f1623ed891
Luna <jeanrosario1703@gmail.com>
2026-01-28 15:01:50 -0500

    Newer Support Beams

--- Levels/Scenes/InventoryChecklist.gd
+++ Levels/Scenes/InventoryChecklist.gd
@@ -4,16 +4,16 @@ extends ItemList
 
 var isPressed:bool = false
 
-func _ready() -> void:
-	for i in range(needed.size()):
-		self.add_item(needed[i].name, needed[i].sprite_2d.texture)
-		
-
-func _process(float) -> void:
-	if (Input.is_action_just_pressed("Inventory")):
-		var layer:CanvasLayer = self.get_parent()
-		if (layer.visible):
-			layer.visible = false
-		else:
-			layer.visible = true
-	
+#func _ready() -> void:
+	#for i in range(needed.size()):
+		#self.add_item(needed[i].name, needed[i].sprite_2d.texture)
+		#
+#
+#func _process(float) -> void:
+	#if (Input.is_action_just_pressed("Inventory")):
+		#var layer:CanvasLayer = self.get_parent()
+		#if (layer.visible):
+			#layer.visible = false
+		#else:
+			#layer.visible = true
+	#
