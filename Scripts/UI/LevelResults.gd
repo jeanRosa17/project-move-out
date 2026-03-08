6c35f329dc9396cd47dabc78b6ef64ce8de46462
ashtriffitt <ashtontriffitt813@gmail.com>
2026-03-08 00:50:45 -0500

    transition audio for all scene swaps

--- Scripts/UI/LevelResults.gd
+++ Scripts/UI/LevelResults.gd
@@ -11,17 +11,25 @@ extends CanvasLayer
 @export var twoStarCompletion: float = 0.50
 @export var threeStarCompletion: float = 1
 
+var transition_audio:AudioStreamPlayer2D
+
 
 func _on_next_level_pressed() -> void:
+	transition_audio = find_child("Level Transition Audio")
 	if (nextLevelName != null):
 		LevelManager.loadScene(nextLevelName)
+		transition_audio.play()
 
 func _on_replay_level_pressed() -> void:
 	var current_scene := get_tree().current_scene
+	transition_audio = find_child("Level Transition Audio")
 	if current_scene:
 		var path := current_scene.scene_file_path
 		LevelManager.loadScene(path)
+		transition_audio.play()
 
 func _on_exit_game_pressed() -> void:
 	#get_tree().quit()
+	transition_audio = find_child("Level Transition Audio")
 	LevelManager.loadScene("res://Scripts/UI/Scenes/TitleScreen.tscn")
+	transition_audio.play()
