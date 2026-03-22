2ff210f0b6fa89df8e4a1368a6779db58d9c3b06
Luna <jeanrosario1703@gmail.com>
2026-03-22 16:13:29 -0400

    Fixed Garage side

--- Scripts/Player Scripts/Controller.gd
+++ Scripts/Player Scripts/Controller.gd
@@ -93,7 +93,6 @@ func handleMovement(_delta:float) -> void:
 	or Input.is_action_pressed("MoveDown")
 	or Input.is_action_pressed("MoveUp")):
 		self.manager.direction = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown", 0.1)
-		print(self.manager.direction)
 		#lockDirection()
 		if (self.manager.getStateName() != "Move" && self.manager.getStateName() != "Lift"):
 			self.manager.changeState("Move")
