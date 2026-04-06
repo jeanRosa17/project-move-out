31efbfa61214adb65f4c35abd9a0fa8e3800884f
Luna <jeanrosario1703@gmail.com>
2026-04-05 22:23:17 -0400

    Made Doorways + Window + Updated Walls

--- Scripts/Teleporter.gd
+++ Scripts/Teleporter.gd
@@ -2,6 +2,7 @@ class_name Teleporter
 extends Area2D
 
 @export var goTo:Teleporter
+@export var furniturePos:Vector2
 @onready var collision:CollisionShape2D = $CollisionShape2D
 
 
@@ -9,7 +10,12 @@ func _on_teleported(body: Node2D) -> void:
 	print("Teleporting to")
 	
 	self.goTo.collision.call_deferred("set_disabled", true)
-	body.call_deferred("set_position", self.goTo.position)
+	if (body is Furniture):
+		body.call_deferred("exitPush")
+		body.call_deferred("set_position", self.goTo.position + self.goTo.furniturePos)
+	else:
+		body.manager.call_deferred("changeState", "Idle")
+		body.call_deferred("set_position", self.goTo.position)
 	
 	await get_tree().create_timer(2).timeout
 	
