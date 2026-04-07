3f5f0c4dfeb98ec5193ea7fab71d6625c8f31f42
Joel Bernardes <jbernardes0229@gmail.com>
2026-04-07 14:18:17 -0400

    Added Timer event
    Should now properly handle if we have a time within the level, end the game after x time )for demo set to 10 secs) and display end screen (wasn't able to test due to PoolLevel not being 100% implemented, but should work on other scenes as needed)

---
+++ Scripts/UI/Scenes/timer.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var label = $Label
+@onready var timer = $Timer
+
+func _ready() -> void:
+	timer.timeout.connect(_on_timer_timeout)
+	timer.start()
+	
+
+func time_left_until_end():
+	var time_left = max(timer.time_left, 0)
+	var minute = floor(time_left / 60)
+	var second = int(time_left) % 60
+	return [minute, second]
+
+func _process(delta: float) -> void:
+	label.text = "%02d:%02d" % time_left_until_end()
+
+func _on_timer_timeout():
+	%HUD.setDialogueTo(DialogueTag.new().create("res://Narrative/GenericDialogue.txt", "endLevel"))
+	%HUD.checkResults()
